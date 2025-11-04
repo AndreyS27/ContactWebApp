2176a3e92cf19a6fba0d351160c8f6068436b26f
Andrey S <salihov.andrey1@yandex.ru>
2025-11-04 09:22:32 +0300

    refactor: refactor IStorage

--- Api/Storage/IStorage.cs
+++ Api/Storage/IStorage.cs
@@ -6,7 +6,7 @@ namespace Api.Storage
     public interface IStorage
     {
         List<Contact> GetContacts();
-        bool Add(Contact contact);
+        Contact Add(Contact contact);
         bool Remove(int id);
         bool UpdateContact(ContactDto contactDto, int id);
     }
