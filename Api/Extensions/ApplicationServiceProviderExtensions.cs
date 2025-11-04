7e795c6b04089b0d031e6a4bf95143896d2ce641
Andrey S <salihov.andrey1@yandex.ru>
2025-11-04 10:39:22 +0300

    feat: add service provider extension method

---
+++ Api/Extensions/ApplicationServiceProviderExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Api.Seed;
+using Api.Storage;
+
+namespace Api.Extensions
+{
+    public static class ApplicationServiceProviderExtensions
+    {
+        public static IServiceProvider AddCustomService(
+            this IServiceProvider services,
+            IConfiguration configuration
+        )
+        {
+            using var scope = services.CreateScope();
+
+            var storage = scope.ServiceProvider.GetService<IStorage>();
+            var dbStorage = storage as ApplicationEfStorage;
+            if (dbStorage != null)
+            {
+                string connectionString = configuration
+                    .GetConnectionString("DefaultConnection");
+
+                new FakerInitializer(connectionString).Initialize();
+            }
+
+            return services;
+        }
+    }
+}
