43b5dbf7f05c3b0499b1c318a9f6d7093b73b44a
Andrey S <salihov.andrey1@yandex.ru>
2025-11-07 14:08:56 +0300

    feat: add pagination method

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,31 @@ import TableContact from "./layout/TableContact/TableContact";
 import { useState, useEffect } from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
 import ContactDetails from "./layout/ContactDetails/ContactDetails";
+import Pagination from "./layout/Pagination/Pagination";
 
 const baseApiUrl = process.env.REACT_APP_API_URL;
 
 const App = () => {
   const [contacts, setContacts] = useState([]);
   const location = useLocation();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [pageSize] = useState(10);
 
-  
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  }
+
   useEffect(() => {
-    const url = `${baseApiUrl}/ContactManagement/contacts`;
+    // /page?pageNumber=2&pageSize=5
+    const url = `${baseApiUrl}/ContactManagement/contacts/page?pageNumber=${currentPage}&pageSize=${pageSize}`;
     axios.get(url).then(
-      res => setContacts(res.data)
+      res => {
+        setContacts(res.data.contacts)
+        setTotalPages(Math.ceil(res.data.totalCount/pageSize))
+      }
     );
-  }, [location.pathname]);
+  }, [currentPage, pageSize, location.pathname]);
 
   const addContact = (contactName, contactEmail) => {
     const newId = contacts.length === 0 ? 1 : Math.max(
@@ -46,6 +57,11 @@ const App = () => {
             <div className="card-body">
               <TableContact
                 contacts={contacts}
+              />
+              <Pagination
+                currentPage={currentPage}
+                totalPages={totalPages}
+                onPageChange={ handlePageChange}
               />
               <FormContact addContact={addContact} />
             </div>
---
+++ client/src/layout/Pagination/Pagination.jsx
@@ -0,0 +1,9 @@
+
+
+const Pagination = () => {
+    return(
+        <>1 2 3 4 ... 10</>
+    );
+}
+
+export default Pagination;
